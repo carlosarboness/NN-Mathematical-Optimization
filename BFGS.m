a67f46230838a1dfa8c60769d14dfdf10523fa17
JoelSoleCasale <101803264+JoelSoleCasale@users.noreply.github.com>
2023-04-02 23:52:36 +0200

    mod GM y BFGS

--- BFGS.m
+++ BFGS.m
@@ -1,28 +1,27 @@
 function [x, k,Lk] = BFGS(x,f,g,epsG,kmax,c1,c2,ialmax,maxiter,eps) 
     n = length(x); I = eye(n);
-    xk = x; H = eye(n); Hk = H; 
-    dk = [];  alk = []; iWk = []; Lk = f(x);  
+    H = I; Lk = f(x);
     k = 1; alpham = 1; 
     while norm(g(x)) > epsG && k < kmax
         d = -H*g(x); 
         if k > 1
             if ialmax == 1
-                alpham = al*(g(xk(:,k-1))'*dk(:,k-1))/(g(x)'*d); 
+                alpham = al*(g(prev_x)'*prev_d)/(g(x)'*d); 
             else 
-                alpham = 2*(f(x)-f(xk(:,k-1)))/(g(x)'*d); 
+                alpham = 2*(f(x)-f(prev_x))/(g(x)'*d); 
             end 
         end 
-        [al, iWc] = uo_BLSNW32(f,g,x,d,alpham,c1,c2,maxiter,eps); 
+        [al, iWc] = uo_BLSNW32(f,g,x,d,alpham,c1,c2,maxiter,eps);
+        prev_x = x; prev_d = d;
         x = x + al*d;
 
-        sk = x - xk(:,k); 
-        yk = g(x)-g(xk(:,k));
+        sk = x - prev_x; 
+        yk = g(x)-g(prev_x);
         rhok = 1/(yk'*sk); 
 
         H = (I - rhok*sk*yk')*H*(I - rhok*yk*sk') + rhok*(sk*sk');
         
-        k = k + 1; 
-        xk = [xk, x]; dk = [dk, d]; alk = [alk, al]; iWk = [iWk, iWc]; Hk(:, :, k) = H;
+        k = k + 1;
         Lk = [Lk f(x)];
     end 
 end
--- GM.m
+++ GM.m
@@ -1,18 +1,18 @@
 function [x, k,Lk] = GM(x,f,g,epsG,kmax,c1,c2,ialmax,maxiter,eps) 
-    xk = x; alk = []; dk = []; iWk = []; Lk = f(x);  
-    k = 1; alpham = 1; 
+    Lk = f(x);  
+    k = 1; alpham = 1;
     while norm(g(x)) > epsG && k < kmax
         d = -g(x); 
         if k > 1
             if ialmax == 1
-                alpham = al*(g(xk(:,k-1))'*dk(:,k-1))/(g(x)'*d); 
+                alpham = al*(g(prev_x)'*prev_d)/(g(x)'*d); 
             else 
-                alpham = 2*(f(x)-f(xk(:,k-1)))/(g(x)'*d); 
+                alpham = 2*(f(x)-f(prev_x))/(g(x)'*d); 
             end 
         end 
-        [al, iWc] = uo_BLSNW32(f,g,x,d,alpham,c1,c2,maxiter,eps); 
-        x = x + al*d; k = k + 1; 
-        xk = [xk, x]; dk = [dk, d]; alk = [alk, al]; iWk = [iWk, iWc];
+        [al, iWc] = uo_BLSNW32(f,g,x,d,alpham,c1,c2,maxiter,eps);
+        prev_x = x; prev_d = d;
+        x = x + al*d; k = k + 1;
         Lk = [Lk f(x)];
     end
 end
