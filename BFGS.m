8877f894d0d10f502ebe5bf5fb7b4227dc1ae903
JoelSoleCasale <101803264+JoelSoleCasale@users.noreply.github.com>
2023-04-16 18:55:18 +0200

    final1

--- BFGS.m
+++ BFGS.m
@@ -14,7 +14,7 @@ function [x,k,Lk,bls_t] = BFGS(x,f,g,epsG,kmax,c1,c2,ialmax,maxiter,eps)
         t1 = clock;
         [al, iWc] = uo_BLSNW32(f,g,x,d,alpham,c1,c2,maxiter,eps);
         t2 = clock;
-        %bls_t = [bls_t etime(t2,t1)];
+        bls_t = [bls_t etime(t2,t1)];
         prev_x = x; prev_d = d;
         x = x + al*d;
 
